7a8316cd3b2407dd3917c3bbbb7a54e445b341f0
dev <dev@localhost.localdomain>
2020-10-27 21:36:27 +0300

    Refactoring player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,38 +1,23 @@
+# Displaying player name
 class Player
-    attr_reader :player, :player_move, :win
-  
-    def initialize
-      @player = player
-      @player_move = player_move
-      @win = win
-    end
-  
-    def player
-     @player
-    end
-  
-    def player_move
-      @move
-     end
-  
-    def win
-      @win
-     end
+  # players name store
+  def initialize
+    @player_name = []
   end
-  player_first = Player.new
-  player_one = player_first.player
-  
-  player_second = Player.new
-  player_two = player_second.player
-  
-  move_first = Player.new
-  move_one = move_first.player_move
-  
-  move_second = Player.new
-  move_two = move_second.player_move
-  
-  win_first = Player.new
-  win_one = win_first.win
-  
-  win_second = Player.new
-  win_two = win_second.win
+
+  def empty_name(name)
+    return 'Empty name can not be accepted. ' if name.empty?
+  end
+
+  # Prompts player to enter his/her name
+  def player_name(name)
+    if @player_name[0].nil?
+      @player_name[0] = name
+      [true, name]
+    elsif @player_name[0] == name
+      [false, "Player name #{name} already taken. Please choose different one."]
+    else
+      [true, name]
+    end
+  end
+end
